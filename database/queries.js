5b7ddf839fa13bd3cbb4cf66a4b1b2925049bf65
Kiefer Ragay <kiefer.m.ragay@gmail.com>
2020-06-06 13:30:52 -0700

    Add Queries to Routes

--- database/queries.js
+++ database/queries.js
@@ -1,4 +1,25 @@
 const mongoose = require('mongoose');
 const Recipe = require('./schema.js');
 
+const addRecipe = (recipe, callback) => {
+  const newRecipe = new Recipe(recipe);
+  newRecipe.save((err, result) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, result);
+    }
+  });
+};
 
+const getRecipes = (callback) => {
+  Recipe.find({}, (err, docs) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, docs);
+    }
+  });
+};
+
+module.exports = { addRecipe, getRecipes};
--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
+const controller = require('../database/queries.js');
+
 const port = process.env.PORT || 3001;
 const public = path.join(__dirname, '../client/dist');
 
@@ -12,11 +14,27 @@ app.listen(port, () => {
 });
 
 app.get('/recipes', (req, res) => {
+  controller.getRecipes((err, recipes) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+    } else {
+      res.status(200).send(recipes);
+    }
+  })
+
 
 });
 
 app.post('/recipes', (req, res) => {
-
+  controller.addRecipe(req.body, (err) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(400);
+    } else {
+      res.sendStatus(400);
+    }
+  });
 });
 
 
