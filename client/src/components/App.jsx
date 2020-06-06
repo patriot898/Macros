66a1e86735d86e09b3e1effe2d6c19e831ba495f
Kiefer Ragay <kiefer.m.ragay@gmail.com>
2020-06-05 20:47:26 -0700

    Create Skeleton for index.jsx, App.jsx

---
+++ client/src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render () {
+    return (
+      <div>Here is the app</div>
+    )
+  }
+}
+
+export default App;
