7d9821f0d20d612277a958d7414c72c564f5a678
Kiefer Ragay <kiefer.m.ragay@gmail.com>
2020-06-06 12:55:14 -0700

    Update Recipe Schema

--- database/schema.js
+++ database/schema.js
@@ -16,6 +16,8 @@ const recipeSchema = new Schema({
   }],
   type: String, // main, shake, side, snack
   group: String, // fruit, vegetable, dairy
+  servings: {type: Number, default: 1},
+  servingSize: { type: String, default: '1 unit' },
   meal: { type: String, default: 'any' }, // breakfast, lunch/dinner, any
   pairing: { type: String, default: 'none' }, // if main: rice, potatoes, etc.
 });
